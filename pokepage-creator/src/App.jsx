b1dbde61dbfc3f7948d4b33e3b08eea4e19f870e
PipitoneSamuele <samuele.pipitone@gmail.com>
2025-08-17 18:48:55 +0200

    header/footer + tailwind

--- pokepage-creator/src/App.jsx
+++ pokepage-creator/src/App.jsx
@@ -4,7 +4,23 @@ function App() {
 
   return (
     <>
-      <p>Hello World!!</p>
+    <div className="flex flex-col min-h-screen">
+      {/* Header */}
+      <header className="bg-blue-600 text-white p-4 shadow">
+        <h1 className="text-2xl font-bold">My Image Grid App</h1>
+      </header>
+
+      {/* Main content */}
+      <main className="flex-1 p-6">
+        <p className="text-gray-700">Upload your images here...</p>
+        {/* your grid component will go here */}
+      </main>
+
+      {/* Footer */}
+      <footer className="bg-gray-100 text-gray-600 text-center p-4">
+        © {new Date().getFullYear()} My App. All rights reserved.
+      </footer>
+    </div>
     </>
   )
 }
